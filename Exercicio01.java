c90e324729ff2ea2e335337ac5fbef25d88dd78c
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-02-07 00:19:15 +0000

    Adição de variaveis

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Exercicio01{
+  public static Scanner input = new Scanner (System.in);
   public static void main (String[] args){
+      int idade;
       System.out.println("Olá, turma");
+      System.out.println("Digite sua idade: ");
+      idade = input.nextInt();
   }
 }
