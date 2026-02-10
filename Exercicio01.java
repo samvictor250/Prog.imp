8ed9e9020816449a7c7242d579d2e3a0a5f07fb0
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-02-10 23:49:48 +0000

    inserção do scanner

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Exercicio01{
-  public static Scanner input = new Scanner (System.in);
+  public static Scanner input = new Scanner(System.in);
   public static void main (String[] args){
       int idade;
       System.out.println("Olá, turma");
