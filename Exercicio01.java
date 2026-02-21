d1190d29a83bfd241e817bedda1d06bf91f8c1df
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-02-21 00:13:29 +0000

    Condicionais com idade

--- Exercicio01.java
+++ Exercicio01.java
@@ -6,12 +6,27 @@ public class Exercicio01{
       int idade;
       System.out.println("Digite sua idade: ");
       idade = input.nextInt();
-
+      
+      // Habilitação 
       if (idade >= 18){
         System.out.println("Você ja pode tirar a carteira de motorista!");
       }
       else{
         System.out.println("Você ainda não pode tirar a carteira de motorista!");
+      }
+
+      // Titulo de eleitor
+      if(idade < 16){
+        System.out.println("Você não pode votar!");
+      }
+      else if(idade > 18){
+        System.out.println("Você é obrigado a votar!");
+      }
+      else if(idade < 70){
+        System.out.println("Você é obrigado a ter o titulo de eleitor e obrigado a votar!");
+      }
+      else{
+        System.out.println("Você não é mais obrigado a voltar");
       }
   }
 }
