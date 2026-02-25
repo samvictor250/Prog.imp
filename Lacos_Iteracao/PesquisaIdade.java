6f5460aa889f301019499032a2bbf299dc9868ba
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-02-25 00:17:31 +0000

    Criação da pesquisa idade

---
+++ Lacos_Iteracao/PesquisaIdade.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PesquisaIdade{
+  public static Scanner input = new Scanner(System.in);
+  public static void main(String[] args){
+    int idade, somaIdades = 0, qtdPessoas = 0;
+    
+    while(qtdPessoas != 0){
+      
+    }
+  }
+}
