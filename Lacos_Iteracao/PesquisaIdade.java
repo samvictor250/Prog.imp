5be67a428ad2bfd1bf3178cf045c8622068444b5
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-02-25 00:24:15 +0000

    Ajusete de espaçamento

--- Lacos_Iteracao/PesquisaIdade.java
+++ Lacos_Iteracao/PesquisaIdade.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class PesquisaIdade{
   public static Scanner input = new Scanner(System.in);
+  
   public static void main(String[] args){
     int idade, somaIdades = 0, qtdPessoas = 0;
     
@@ -9,4 +10,5 @@ public class PesquisaIdade{
       
     }
   }
+
 }
